da3ffb059c83f0ba44d1f002b28a9842d9e55cbf
Jack <jpick329@gmail.com>
2022-08-06 11:15:16 -0500

    init commit

---
+++ src/main/java/com/jackpickus/App.java
@@ -0,0 +1,16 @@
+package com.jackpickus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        int x = 5;
+        x += 55;
+        System.out.println(x);
+    }
+}
