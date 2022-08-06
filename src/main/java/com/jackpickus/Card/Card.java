d515edb512099d9de913de97d468e1fbc841048a
Jack <jpick329@gmail.com>
2022-08-06 11:20:13 -0500

    added vars for card pojo

--- src/main/java/com/jackpickus/Card/Card.java
+++ src/main/java/com/jackpickus/Card/Card.java
@@ -1,5 +1,44 @@
 package com.jackpickus.Card;
 
+import com.jackpickus.Suit.Suit;
+
 public class Card {
-    
+    private String name;
+    private int value;
+    private Suit suit;
+
+    public Card(String name, int value, Suit suit) {
+        this.name = name;
+        this.value = value;
+        this.suit = suit;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public void setSuit(Suit suit) {
+        this.suit = suit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name + " of " + suit;
+    }
 }
