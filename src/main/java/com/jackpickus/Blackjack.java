732fefb9f206aefca162bf78bccd00d42020ef11
Jack <jpick329@gmail.com>
2022-08-07 10:24:12 -0500

    changed App.java to Blackjack.java

---
+++ src/main/java/com/jackpickus/Blackjack.java
@@ -0,0 +1,19 @@
+package com.jackpickus;
+
+/**
+ * Hello world!
+ *
+ */
+public class Blackjack {
+
+    public static void main( String[] args ) {
+        Blackjack blackjack = new Blackjack();
+        blackjack.playGame();
+    }
+
+    private void playGame() {
+
+
+
+    }
+}
